5aef8fd544cec552ed2c2516d888c875a8fd0294
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-04-14 21:43:01 +0600

    feat: Add initial implementation for A. Watermelon problem and configuration files

---
+++ div_4/A_Watermelon.c
@@ -0,0 +1,13 @@
+
+#include<stdio.h>
+int main(){
+    int tormuj;
+    scanf("%d",&tormuj);
+    if(tormuj%2==0 && tormuj/2>=2){
+
+
+        printf("YES\n");
+    }else{
+        printf("NO\n");
+    }
+}
